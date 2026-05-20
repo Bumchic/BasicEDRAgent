d05bcf23d71d1dba3df92849169c4cf04ad8c9d0
Bumchic <duonganhminh05@gmail.com>
2026-05-20 18:58:21 +0700

    added code to read evtx

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from Evtx import Evtx
+from os import path, environ
+
 def main():
-    print("Hello from basicedragent!")
+    try:
+        with Evtx.Evtx(filename=path.join(environ['USERPROFILE'], 'Desktop', 'sysmon', 'Microsoft-Windows-Sysmon%4Operational.evtx')) as log:
+            header = log.get_file_header()
+            print('<Events>')
+            for record in log.records():
+                print(record.xml())
+            print('</Events>')
+
+        
+        
+    except Exception as e:
+        print(f"Error: {e}")
+    
+
+        
+
+    
 
 
 if __name__ == "__main__":
