830b5bdee3a1cd774977d5e848b1e653ca1ac0cc
Bumchic <duonganhminh05@gmail.com>
2026-05-21 02:30:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from requests import post
 def main():
     sysmon_dest_path = path.join(environ['USERPROFILE'], 'Desktop', 'sysmon')
     sysmon_file_name = 'Microsoft-Windows-Sysmon%4Operational.evtx'
-    sysmon_src_path = path.abspath('C:\Windows\System32\winevt\Logs\Microsoft-Windows-Sysmon%4Operational.evtx')
+    sysmon_src_path = path.abspath('C:\Windows\System32\winevt\Logs')
     try: 
         robocopy.copy(sysmon_src_path, sysmon_dest_path, sysmon_file_name)
         with Evtx.Evtx(filename=path.join(sysmon_dest_path, sysmon_file_name)) as log:
             header = log.get_file_header()
             print('<Events>')
             for record in log.records():
-                print(record.xml())
                 payload = {"event": record.xml()}
-                post('http://127.0.0.1:8000/api/event', )
+                post('http://127.0.0.1:8000/api/event', data = payload)
+                break
             print('</Events>')
 
         
