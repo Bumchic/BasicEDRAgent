daa1589906190fb5627e50e13cd68cd9a5d8a146
Bumchic <duonganhminh05@gmail.com>
2026-05-21 01:29:47 +0700

    added robocopy code to copy

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from Evtx import Evtx
 from os import path, environ
+from robocopy import robocopy
 
 def main():
-    try:
-        with Evtx.Evtx(filename=path.join(environ['USERPROFILE'], 'Desktop', 'sysmon', 'Microsoft-Windows-Sysmon%4Operational.evtx')) as log:
+    sysmon_dest_path = path.join(environ['USERPROFILE'], 'Desktop', 'sysmon')
+    sysmon_file_name = 'Microsoft-Windows-Sysmon%4Operational.evtx'
+    sysmon_src_path = path.abspath('C:\Windows\System32\winevt\Logs\Microsoft-Windows-Sysmon%4Operational.evtx')
+    try: 
+        robocopy.copy(sysmon_src_path, sysmon_dest_path, sysmon_file_name)
+        with Evtx.Evtx(filename=path.join(sysmon_dest_path, sysmon_file_name)) as log:
             header = log.get_file_header()
             print('<Events>')
             for record in log.records():
