dbf2222e14db63bd3a349f22bd522821d84fcffd
Bumchic <duonganhminh05@gmail.com>
2026-05-21 18:35:34 +0700

    added while true loop for continuous running

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ from Evtx import Evtx
 from os import path, environ
 from robocopy import robocopy
 from requests import post
+from time import sleep
 
 def main():
     sysmon_dest_path = path.join(environ['USERPROFILE'], 'Desktop', 'sysmon')
     sysmon_file_name = 'Microsoft-Windows-Sysmon%4Operational.evtx'
-    sysmon_src_path = path.abspath('C:\Windows\System32\winevt\Logs')
+    sysmon_src_path = path.abspath('C:\\Windows\\System32\\winevt\\Logs')
     try: 
-        robocopy.copy(sysmon_src_path, sysmon_dest_path, sysmon_file_name)
-        with Evtx.Evtx(filename=path.join(sysmon_dest_path, sysmon_file_name)) as log:
-            header = log.get_file_header()
-            print('<Events>')
-            for record in log.records():
-                payload = {"event": record.xml()}
-                post('http://127.0.0.1:8000/api/event', data = payload)
-                break
-            print('</Events>')
+        while True:
+            robocopy.copy(sysmon_src_path, sysmon_dest_path, sysmon_file_name)
+            with Evtx.Evtx(filename=path.join(sysmon_dest_path, sysmon_file_name)) as log:
+                header = log.get_file_header()
+                print('<Events>')
+                for record in log.records():
+                    event = record.xml()
+                    payload = {"event": event}
+                    post('http://127.0.0.1:8000/api/event', json = payload)
+                print('</Events>')
+                sleep(30)
 
         
         
