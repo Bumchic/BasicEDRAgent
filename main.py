1b74d2f54c02b1673572d2f675cf25e9b39c9b26
Bumchic <duonganhminh05@gmail.com>
2026-05-23 01:37:30 +0700

    Change conncection method and various dependencies
    - Changed sending with post to making connection using websocket
    - parse sysmon xml into beautifulsoup format
    - send to websocket using json

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
+import websockets
 from Evtx import Evtx
 from os import path, environ
+from lxml import etree
 from robocopy import robocopy
-from requests import post
+from requests import Session, post
 from time import sleep
+from bs4 import BeautifulSoup
+import asyncio
+import json
 
-def main():
+
+async def main():
     sysmon_dest_path = path.join(environ['USERPROFILE'], 'Desktop', 'sysmon')
     sysmon_file_name = 'Microsoft-Windows-Sysmon%4Operational.evtx'
     sysmon_src_path = path.abspath('C:\\Windows\\System32\\winevt\\Logs')
+    keyword = ['whoami', 'tasklist', 'quser']
+    extension = ['.bat', '.cmd', '.rar']
     try: 
-        while True:
-            robocopy.copy(sysmon_src_path, sysmon_dest_path, sysmon_file_name)
-            with Evtx.Evtx(filename=path.join(sysmon_dest_path, sysmon_file_name)) as log:
-                header = log.get_file_header()
-                print('<Events>')
-                for record in log.records():
-                    event = record.xml()
-                    payload = {"event": event}
-                    post('http://127.0.0.1:8000/api/event', json = payload)
-                print('</Events>')
-                sleep(30)
+        async with websockets.connect("ws://127.0.0.1:8000/ws") as socket:
+            while True:
+                robocopy.copy(sysmon_src_path, sysmon_dest_path, sysmon_file_name)
+                with Evtx.Evtx(filename=path.join(sysmon_dest_path, sysmon_file_name)) as log:
+                    # header = log.get_file_header()
+                    for record in log.records():
+                        event = record.lxml()
+                        soup = BeautifulSoup(etree.tostring(event), features="xml")    
+                        payload = {"event": soup.__str__()}
+                        await socket.send(message=json.dumps(payload), text= True)
+                        break
+                    break
+                    sleep(30)
+                break
 
         
         
@@ -33,4 +44,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
