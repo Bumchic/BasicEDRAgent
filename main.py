0e1be1dbfe9179d794eb3d6057d943951711b663
Bumchic <duonganhminh05@gmail.com>
2026-05-26 02:53:39 +0700

    added auth and change how evtx reader work
    - User now need to enter credential to get token from server
    - change how evtx reader work, no more robocopy

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import requests
 from Evtx import Evtx
 from os import path, environ
 from lxml import etree
-from robocopy import robocopy
 from requests import Session, post
 from time import sleep
 from bs4 import BeautifulSoup
@@ -12,49 +11,43 @@ import json
 import hashlib
 
 
+
 async def main():
-    sysmon_dest_path = path.join(environ['USERPROFILE'], 'Desktop', 'sysmon')
-    sysmon_file_name = 'Microsoft-Windows-Sysmon%4Operational.evtx'
-    sysmon_src_path = path.abspath('C:\\Windows\\System32\\winevt\\Logs')
-    keyword = ['whoami', 'tasklist', 'quser']
-    extension = ['.bat', '.cmd', '.rar']
-    username = input()
-    password = str(input())
-    passwordhashed = hashlib.sha1(password.encode('utf-8'))
-    try: 
-        cred = {
-            'username': username,
-            'password': passwordhashed
-        }
-        requests.post(url='http://127.0.0.1:8000', json=cred)
-        async with websockets.connect(f"ws://127.0.0.1:8000/ws?token=123") as socket:
-            while True:
-                robocopy.copy(sysmon_src_path, sysmon_dest_path, sysmon_file_name)
-                with Evtx.Evtx(filename=path.join(sysmon_dest_path, sysmon_file_name)) as log:
+    sysmon_dest_path = path.join(environ["USERPROFILE"], "Desktop", "sysmon")
+    sysmon_file_name = "Microsoft-Windows-Sysmon%4Operational.evtx"
+    sysmon_src_path = path.abspath("C:\\Windows\\System32\\winevt\\Logs")
+    keyword = ["whoami", "tasklist", "quser"]
+    extension = [".bat", ".cmd", ".rar"]
+    username = input("input username: ")
+    password = input("input password: ")
+    passwordhashed = hashlib.sha1(password.encode("utf-8"))
+    try:
+        cred = {"username": username, "password": passwordhashed.hexdigest()}
+        res = requests.post(url="http://127.0.0.1:8000/auth", json=cred)
+        if res.status_code != 200:
+            raise Exception("credential invalid")
+        message = res.json()
+        token = message["message"]
+        print(f"token received: {token}")
+        async with websockets.connect(
+            f"ws://127.0.0.1:8000/ws?token={token}"
+        ) as socket:
+            with Evtx.Evtx(
+                filename=path.join(sysmon_src_path, sysmon_file_name)
+            ) as log:
+                while True:
                     # header = log.get_file_header()
-                    count = 0
                     for record in log.records():
                         event = record.lxml()
-                        soup = BeautifulSoup(etree.tostring(event), features="xml")    
+                        soup = BeautifulSoup(etree.tostring(event), features="xml")
                         payload = {"event": soup.__str__()}
-                        await socket.send(message=json.dumps(payload), text= True)
-                        if count > 300:
-                            break
-                        count = count + 1
+                        await socket.send(message=json.dumps(payload), text=True)
+                        break
                     break
-                    sleep(30)
-                break
-        await socket.close()
+            await socket.close()
 
-        
-        
     except Exception as e:
         print(f"Error: {e}")
-    
-
-        
-
-    
 
 
 if __name__ == "__main__":
