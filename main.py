8aafe2bb40ed81c5258ae927e04ec304fb024edb
Bumchic <duonganhminh05@gmail.com>
2026-06-02 22:08:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,41 +11,46 @@ import json
 import hashlib
 
 
+def get_token(username: str, password: str) -> websockets.ClientConnection:
+    cred = {"username": username, "password": password}
+    res = requests.post(url="http://127.0.0.1:8000/auth", json=cred)
+    if res.status_code != 200:
+        raise Exception("credential invalid")
+    message = res.json()
+    token = message["message"]
+    print(f"token received: {token}")
+    return token
+
 
 async def main():
-    sysmon_dest_path = path.join(environ["USERPROFILE"], "Desktop", "sysmon")
     sysmon_file_name = "Microsoft-Windows-Sysmon%4Operational.evtx"
     sysmon_src_path = path.abspath("C:\\Windows\\System32\\winevt\\Logs")
-    keyword = ["whoami", "tasklist", "quser"]
-    extension = [".bat", ".cmd", ".rar"]
     username = input("input username: ")
     password = input("input password: ")
     passwordhashed = hashlib.sha1(password.encode("utf-8"))
+    passwordhashedhex = passwordhashed.hexdigest()
+    #token = get_token(username=username, password=passwordhashedhex)
     try:
-        cred = {"username": username, "password": passwordhashed.hexdigest()}
-        res = requests.post(url="http://127.0.0.1:8000/auth", json=cred)
-        if res.status_code != 200:
-            raise Exception("credential invalid")
-        message = res.json()
-        token = message["message"]
-        print(f"token received: {token}")
-        async with websockets.connect(
-            f"ws://127.0.0.1:8000/ws?token={token}"
-        ) as socket:
-            with Evtx.Evtx(
-                filename=path.join(sysmon_src_path, sysmon_file_name)
-            ) as log:
-                while True:
-                    # header = log.get_file_header()
-                    for record in log.records():
-                        event = record.lxml()
-                        soup = BeautifulSoup(etree.tostring(event), features="xml")
-                        payload = {"event": soup.__str__()}
-                        await socket.send(message=json.dumps(payload), text=True)
-                        break
-                    break
-            await socket.close()
-
+        async for socket in websockets.connect(f"ws://127.0.0.1:8000/ws?token={get_token(username=username, password=passwordhashedhex)}"):
+            try:
+                with Evtx.Evtx(
+                        filename=path.join(sysmon_src_path, sysmon_file_name)
+                    ) as log:
+                    while True:
+                            # header = log.get_file_header()
+                            print('reading logs...')
+                            for record in log.records():
+                                event = record.lxml()
+                                soup = BeautifulSoup(etree.tostring(event), features="xml")
+                                payload = {"event": soup.__str__()}
+                                await socket.send(message=json.dumps(payload), text=True)
+                                await socket.recv()
+                            break
+            except websockets.exceptions.ConnectionClosed as e:
+                print('diconnected exception: ' + e.__str__())
+                print('attempting reconnection')
+                sleep(5)
+                continue
     except Exception as e:
         print(f"Error: {e}")
 
